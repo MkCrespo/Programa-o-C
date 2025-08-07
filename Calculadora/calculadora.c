23c30169ae07fd9a824246fd88ae7448c7414107
Mike <mikeoliveira1011@gmail.com>
2025-08-07 19:55:39 -0300

    Add files via upload

---
+++ Calculadora/calculadora.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+ 
+int main() {
+    int num1, num2, soma;
+ 
+    // Lendo dois números do usuário
+    printf("Digite o primeiro numero: ");
+    scanf("%d", &num1);
+ 
+    printf("Digite o segundo numero: ");
+    scanf("%d", &num2);
+ 
+    // Calculando a soma dos dois números
+    soma = num1 + num2;
+ 
+    // Exibindo o resultado
+    printf("A soma de %d e %d: %d\n", num1, num2, soma);
+    scanf("%d", &soma);
+ 
+    return 0;
+}
